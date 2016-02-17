19088a3aa22883d57b9e70e580e70572f2e63a53
Greg Baker <ggbaker@sfu.ca>
2016-02-17 15:28:06 -0800

    other events section

---
+++ _sources/event2.js
@@ -0,0 +1,9 @@
+mouse_arrived = function() {…}
+mouse_left = function() {…}
+setup = function() {
+  jQuery("#hoverable").mouseenter(mouse_arrived);
+  jQuery("#hoverable").mouseleave(mouse_left);
+}
+jQuery(document).ready(setup);
+
+
---
+++ _sources/event4.js
@@ -0,0 +1,4 @@
+jQuery(document).on('scroll', user_scrolled);
+jQuery("p").on('touchstart', handle_touch);
+
+
